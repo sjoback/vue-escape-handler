0ec22d313d6b792d50e224569e7a7cef4c5fec35
Joakim Sjöbäck <31646325+sjoback@users.noreply.github.com>
2020-12-08 13:02:23 +0100

    version & index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// function Escape () {
+//
+// }
+
+export default {
+    created() {
+        let that = this;
+
+        document.addEventListener('keyup', function (evt) {
+            if (evt.keyCode === 27) {
+                that.close();
+            }
+        });
+    },
+};
